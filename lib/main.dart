58b51fb37db24c3c1cd674eaa416249d5da14eee
Basit <basitali2619@gmail.com>
2022-06-03 10:39:35 +0500

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+// write stl 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+          child: Text("welcome to 30 days of flutter"),
+          ),
+        ),
+      ),
+    );
+  }
+}
